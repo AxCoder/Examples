174b6249dddaf164c028e2ff96d83f66f20db3da
AxCoder <belugin@outlook.com>
2023-06-10 21:52:36 +0300

    StructsAndNullable Example

---
+++ StructsAndNullable/src/NullableStructsTransformer/NullableStructsTransformer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Transformers;
+
+public class NullableStructsTransformer
+{
+    public static TResult? Transform<TSource, TResult>(
+        TSource? source,
+        Func<TSource, TResult> transformation)
+        => source is null
+            ? default
+            : transformation(source);
+
+    public static TResult? Transform<TSource, TResult>(
+        TSource? source,
+        Func<TSource, TResult> transformation)
+        where TSource : struct
+    {
+        if (source is null)
+        {
+            return default;
+        }
+        else
+        {
+            return transformation(source.Value);
+        }
+    }
+
+    public static TResult? TransformFixed<TSource, TResult>(
+        TSource? source,
+        Func<TSource, TResult> transformation)
+        where TSource : struct
+        where TResult : struct
+    {
+        if (source is null)
+        {
+            return default;
+        }
+        else
+        {
+            return transformation(source.Value);
+        }        
+    }
+}
+
---
+++ StructsAndNullable/tests/NullableStructsTransformerTests/NullableStructsTransformerTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace Transformers.Tests;
+
+public class NullableStructsTransformerTests
+{
+    private const string TestSuffix = "Test";
+
+    [Fact]
+    public void Transform_IfNotNullReference_AppliesTransformation()
+    {
+        const string source = nameof(source);
+        NullableStructsTransformer.Transform(source, x => x + TestSuffix).Should().Be(source + TestSuffix);
+    }
+
+    [Fact]
+    public void Transform_IfNullReference_ReturnsNull()
+    {
+        const string? source = null;
+        NullableStructsTransformer.Transform(source, x => x + TestSuffix).Should().BeNull();
+    }
+
+    [Fact]
+    public void Transform_IfNotNullStruct_AppliesTransformation()
+    {
+        DateTime source = DateTime.Now;
+        NullableStructsTransformer.Transform(source, x => x.AddDays(1))
+            .Should().Be(source.AddDays(1));
+    }
+
+    [Fact]
+    public void Transform_IfNullStruct_ReturnsNull()
+    {
+        DateTime? source = null;
+        DateTime? result = NullableStructsTransformer.Transform(source, x => x.AddDays(1));
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public void TransformFixed_IfNullStruct_ReturnsNull()
+    {
+        DateTime? source = null;
+        DateTime? result = NullableStructsTransformer.TransformFixed(source, x => x.AddDays(1));
+        result.Should().BeNull();
+    }
+}
